716b4054a7c82ec08a0271804a4ebc20e1c93fed
dev-manager <dev-manager@dev.manager.github.com>
2021-03-16 17:38:40 +0900

    히힣

---
+++ main.py
@@ -0,0 +1,12 @@
+
+def star(x):
+    for i in range(1, x):
+        print('　' * (x - i), end='')
+        print('고소' * i, end='')
+        print('\n')
+    for j in range(x):
+        print('　' * (j), end='')
+        print('고소' * (x - j), end='')
+        print('\n')
+if __name__ == '__main__':
+    star(10)
