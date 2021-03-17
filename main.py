b67dcaec6676d9c2156059828ed57fc0f849c253
dev-manager <dev-manager@dev.manager.github.com>
2021-03-17 17:33:13 +0900

    add class

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-
-def star(x):
-    for i in range(1, x):
-        print('　' * (x - i), end='')
-        print('고소' * i, end='')
-        print('\n')
-    for j in range(x):
-        print('　' * j, end='')
-        print('고소' * (x - j), end='')
-        print('\n')
+class Slack:
+    @staticmethod
+    def star(x, text):
+        for i in range(1, x):
+            print('　' * (x - i), end='')
+            print(text * i, end='')
+            print('\n')
+        for j in range(x):
+            print('　' * j, end='')
+            print(text * (x - j), end='')
+            print('\n')
 
 
 if __name__ == '__main__':
-    star(10)
+    Slack.star(10, '고소')
